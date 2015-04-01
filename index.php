e663d77a8845c93e5637bbbdd30b2e3bd24075e4
Ravioli Morales <shalorah@gmail.com>
2015-04-01 00:31:09 -0400

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+$randomThings = array(
+    'Jesus Christ',    
+    'yolo swag',    
+    'lord satan',    
+    'fucker',    
+    'help me',    
+    'Sebastian',    
+    'damned',
+    'America',
+    'van Gogh',
+    'fuckbot',
+    'fuckboy',
+    '666',
+    'head honcho',
+    'slime queen',
+    'Porpentine',
+    'Abraham Lincoln',
+    'goddess',
+    'Macbeth',
+    'Isabella',
+    'William Shakespeare',
+    'satan',
+    'bill clinton',
+    'sweg',
+    'bigmac',
+    'Zac',
+    'rumplestiltskin',
+);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
