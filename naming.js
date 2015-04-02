d4ac1465b6067109e3dbfb60a721b164817ad381
the ravioli from hell <shalorah@gmail.com>
2015-04-01 23:19:49 -0400

    Update naming.js

--- naming.js
+++ naming.js
@@ -1,11 +1,12 @@
 var nameIdea = new Array ();
 nameIdea[0] = "Macbeth";
 nameIdea[1] = "Skeletor";
-nameIdea[2] = "lil fricker";
+nameIdea[2] = "lil fucker";
 nameIdea[3] = "square pasta bastard";
 nameIdea[4] = "bush staged 9/11";
-nameIdea[5] = "Zayn Malik";
+nameIdea[5] = "anusthrasher69";
 nameIdea[6] = "xXx_y0l0_360_sWaGsCoPe_xXx";
-var i = Math.floor(7*Math.random())
+nameIdea[7] = "it's not ogre";
+var i = Math.floor(8*Math.random())
 
 document.write(nameIdea[i]);
