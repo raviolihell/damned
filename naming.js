484aac72f43b930b022fc8e5f4075b5a610b4dbd
the ravioli from hell <shalorah@gmail.com>
2015-04-01 23:01:41 -0400

    Update naming.js

--- naming.js
+++ naming.js
@@ -1,7 +1,7 @@
-var array = ["satann", "Drizzy", "macbeth", "badonkadonk", "Slime Jr.", "Skeletor"];
-
-var randInt = randomGenerator(0, array.length - 1);
-
-var item = array[randInt];
-
-return;
+var msgArr = new Array();
+	msgArr[0] = "Message 1...";
+	msgArr[1] = "Message 2...";
+	msgArr[2] = "Message 3..."
+	window.onload = function () {
+		document.getElementById("msgDiv").innerHTML = msgArr[Math.floor(Math.random()*msgArr.length)];
+	}
