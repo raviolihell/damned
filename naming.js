784415a90a4b999e5a8b1ba1293c75564eaa01ed
the ravioli from hell <shalorah@gmail.com>
2015-04-01 23:11:01 -0400

    Update naming.js

--- naming.js
+++ naming.js
@@ -1,13 +1,11 @@
-var words = ['Horse',
-'Pig',
-'Dog',
-'Cat',
-'Parrot',
-'Iguana'
-];
-function randomWord(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
+var nameIdea = new Array ();
+nameIdea[0] = "All the leaves are brown";
+nameIdea[1] = "And the sky is grey";
+nameIdea[2] = "I've been for a walk";
+nameIdea[3] = "On a winter's day";
+nameIdea[4] = "I'd be safe and warm";
+nameIdea[5] = "If I was in L.A.";
+nameIdea[6] = "California dreaming, On such a winter's day";
+var i = Math.floor(7*Math.random())
 
-for(var x=0; x<20; x++)
-    console.log(randomWord(words));
+document.write(r_text[i]);
