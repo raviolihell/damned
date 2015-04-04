7801a9a9bc34cce9e997fea88032bfa9940ce5ef
the ravioli from hell <shalorah@gmail.com>
2015-04-03 21:34:29 -0400

    Update serve.php

--- serve.php
+++ serve.php
@@ -1,3 +1,11 @@
-<?PHP
-congrats fucknuts
-?>
+<!DOCTYPE HTML>
+<head>
+<link rel="stylesheets" type="text/css" src="satan.css">
+</head>
+
+<body>
+Congrats fucknuts.
+<p> <?php echo $_POST['name']; ?> has been born.
+
+</body>
+</html>
