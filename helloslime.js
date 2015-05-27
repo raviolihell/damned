80ac199556fd7745a5f7b796c87b3d3be3a8f80e
the ravioli from hell <shalorah@gmail.com>
2015-05-26 21:39:13 -0400

    Update helloslime.js

--- helloslime.js
+++ helloslime.js
@@ -4,10 +4,14 @@ hi[1] = "i worship satan";
 hi[2] = "i like to rhyme";
 hi[3] = "ravioli is my passion";
 hi[4] = "jet fuel can't melt steel beams";
-hi[5] = "i sleep with numerous stuffed animals";
+hi[5] = "i sleep with stuffed animals";
 hi[6] = "i study electrical engineering";
 hi[7] = "this is my swamp";
 hi[8] = "you can't see me";
-var i = Math.floor(9*Math.random())
+hi[9] = "my glasses are smudged";
+hi[10] = "you should email me";
+hi[11] = "it's past my bedtime";
+hi[12] = "this site has no purpose";
+var i = Math.floor(13*Math.random())
 //this number ^ must always be one more than however many greetings there are!!!
 document.write(hi[i]);
